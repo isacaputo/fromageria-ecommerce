6ebe1d2cea3085bc464ba9846699463b9a494de3
isacaputo <isadora.caputo@gmail.com>
2025-01-27 12:45:05 +0100

    add theme provider (with hydration error)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-5">
+      <div className="w-100 flex justify-end">
+        <ThemeToggle />
+      </div>
       <h1 className="text-blue-500 font-josefin">Home</h1>
       <Button variant={'outline'}>Clique Aqui</Button>
     </div>
