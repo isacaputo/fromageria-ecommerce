525ed18381881532b949dab2030f4794dacd21f3
isacaputo <isadora.caputo@gmail.com>
2025-01-27 12:01:12 +0100

    install next themes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <h1 className="text-blue-500 font-josefin">Home</h1>
+      <Button variant={'outline'}>Clique Aqui</Button>
     </div>
   );
 }
