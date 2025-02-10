c1554b3fa65ba45db73f38212c81856f1e8c5bd7
isacaputo <isadora.caputo@gmail.com>
2025-02-10 12:00:54 +0100

    update sidebar logo

--- src/components/dashboard/sidebar/sidebar.tsx
+++ src/components/dashboard/sidebar/sidebar.tsx
@@ -9,7 +9,7 @@ interface SidebarProps {
 }const Sidebar: FC<SidebarProps> = async ({ isAdmin }) => {
   const user = await currentUser();
   return <div className='w-[300px] border-r h-screen p-4 flex flex-col fixed top-0 left-0 bottom-0'>
-    <Logo width='100%' height='180px' />
+    <Logo width='100%' height='100px' />
     <span className='mt-3' />
     {user && <UserInfo user={user} />}
   </div>
