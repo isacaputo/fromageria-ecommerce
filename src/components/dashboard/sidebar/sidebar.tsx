50fe9868fa39f9e5c139f096cf350c9f90733cb9
isacaputo <isadora.caputo@gmail.com>
2025-02-13 16:41:54 +0100

    finish sidebar component

--- src/components/dashboard/sidebar/sidebar.tsx
+++ src/components/dashboard/sidebar/sidebar.tsx
@@ -2,16 +2,19 @@ import Logo from "@/components/shared/logo";
 import UserInfo from "./userInfo";
 import { currentUser } from "@clerk/nextjs/server";
 import { FC } from "react";
-
-
+import SidebarNavAdmin from "./navAdmin";
+import { adminDashboardSidebarOptions } from "@/constants/data";
 interface SidebarProps {
   isAdmin?: boolean;
-}const Sidebar: FC<SidebarProps> = async ({ isAdmin }) => {
+}
+
+const Sidebar: FC<SidebarProps> = async ({ isAdmin }) => {
   const user = await currentUser();
   return <div className='w-[300px] border-r h-screen p-4 flex flex-col fixed top-0 left-0 bottom-0'>
     <Logo width='100%' height='80px' />
     <span className='mt-3' />
     {user && <UserInfo user={user} />}
+    {isAdmin && <SidebarNavAdmin menuLinks={adminDashboardSidebarOptions} />}
   </div>
 }
 
